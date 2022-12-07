00d90444d3ff7eeeb320ab880f6a6b83e2f2dd39
DongwoonJeong <dongwoon.dj@gmail.com>
2022-12-06 18:42:40 -0600

    Update application_views.js

--- src/application_views.js
+++ src/application_views.js
@@ -5,7 +5,7 @@ var account = [
   { userId: "2", PIN: "2123", balance: 100.0 },
 ];
 var history = [];
-var userNum =0;
+var userNum = 0;
 const functions = {
   menu: function () {
     var rl = readline.createInterface(process.stdin, process.stdout);
@@ -74,10 +74,8 @@ const functions = {
       const parsed = parseInt(input);
       switch (parsed) {
         case 1:
-          console.log("4444444441");
           functions.balanceCheck();
           rl.close();
-          
 
         case 2:
           console.log("2");
@@ -118,35 +116,63 @@ const functions = {
   },
   balanceCheck: function () {
     var rl = readline.createInterface(process.stdin, process.stdout);
-    console.log('');
-    console.log(' BALANCE: $' + account[userNum].balance);
-    console.log('');
+    console.log("");
+    console.log(" BALANCE: $" + account[userNum].balance);
+    console.log("");
     rl.close();
     functions.userMenu();
   },
-  History: function () {
-
-  },
-  updatePin: function () {
-
-  },
+  History: function () {},
+  updatePin: function () {},
   Withdraw: function () {
-
+    var rl = readline.createInterface(process.stdin, process.stdout);
+    rl.question("Enter withdraw amount: ", (amount) => {
+      if (amount < 0) {
+        console.log("Deposit amount should be more then 0.");
+        rl.close();
+        functions.Deposit();
+      } else {
+        account[userNum].balance -= parseInt(amount);
+        console.log(`Balance: ${account[userNum].balance}`);
+        history.push({
+          userId: account[userNum].userId,
+          balance: account[userNum].balance,
+          message:
+            "Withdraw Amount of $" +
+            amount +
+            ". Balance = " +
+            account[userNum].balance,
+        });
+      }
+      rl.close();
+      functions.userMenu();
+    });
   },
   Deposit: function () {
-        var rl = readline.createInterface(process.stdin, process.stdout);
-        rl.question("Enter deposit amount: ", (amount) => {
-            if(amount < 0){
-                console.log('Deposit amount should be more then 0.');
-                rl.close();
-                functions.Deposit();
-            }else{
-                account[userNum].balance += parseInt(amount);
-                console.log(`Balance: ${account[userNum].balance}`);
-                history.push({userId: account[userNum].userId, balance: account[userNum].balance, message: 'Deposited Amount of $' + amount + ' to account ' + account[userNum].userId +  '. Balance = $' + account[userNum].balance});            }
-                rl.close();
-                functions.userMenu();
-  });
-}
-}
+    var rl = readline.createInterface(process.stdin, process.stdout);
+    rl.question("Enter deposit amount: ", (amount) => {
+      if (amount < 0) {
+        console.log("Deposit amount should be more then 0.");
+        rl.close();
+        functions.Deposit();
+      } else {
+        account[userNum].balance += parseInt(amount);
+        console.log(`Balance: ${account[userNum].balance}`);
+        history.push({
+          userId: account[userNum].userId,
+          balance: account[userNum].balance,
+          message:
+            "Deposited Amount of $" +
+            amount +
+            " to account " +
+            account[userNum].userId +
+            ". Balance = $" +
+            account[userNum].balance,
+        });
+      }
+      rl.close();
+      functions.userMenu();
+    });
+  },
+};
 module.exports = functions;
